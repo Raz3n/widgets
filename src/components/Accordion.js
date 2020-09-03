8e516d59064179c66653cf4ee6431132b24745f2
Nelson <nbritob@hotmail.com>
2020-09-03 15:53:53 +0100

    adds active index useState hooks

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  const renderItems = items.map((item) => {
+  const [activeIndex, setActiveindex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveindex(index);
+  };
+
+  const renderItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderItems}
+    </div>
+  );
 };
 
 export default Accordion;
