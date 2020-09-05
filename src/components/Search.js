c81cf303854f273c9564a740babd2fdca6b9192c
Nelson <nbritob@hotmail.com>
2020-09-05 15:18:14 +0100

    adds cleanup function in useeffect hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,6 @@ const Search = () => {
   const [term, setTerm] = useState("programming");
   const [results, setResults] = useState([]);
 
-  console.log(results);
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -20,14 +19,29 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
         <div className="right floated content">
-          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
             Go
           </a>
         </div>
