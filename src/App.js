2ad349fa02fe09e8cf25471b6ac4153dd35c0dd1
Nelson <nbritob@hotmail.com>
2020-09-05 22:22:06 +0100

    adds Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
+    title: "What is React?",
+    content: "React is a front end javascript framework",
   },
   {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
   },
   {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Colour Red",
+    value: "red",
+  },
+  {
+    label: "The Colour Green",
+    value: "green",
+  },
+  {
+    label: "The Colour Blue",
+    value: "blue",
   },
 ];
 
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options}/>
     </div>
   );
 };
