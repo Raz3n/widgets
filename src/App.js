27c6813f0905eee6574175069993a9be7d95c582
Nelson <nbritob@hotmail.com>
2020-09-05 14:18:25 +0100

    adds useState hook for results from api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
-    title: "What is React?",
-    content: "React is a front end javascript framework",
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
   },
   {
-    title: "Why use React?",
-    content: "React is a favourite JS library amongst engineers",
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
   },
   {
-    title: "How do you use React?",
-    content: "You use React by creating components",
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
   },
 ];
 
