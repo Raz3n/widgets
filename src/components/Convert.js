e5ee9ca0eae7a5c486c51cc0802c04098d3cc286
Nelson <nbritob@hotmail.com>
2020-09-08 11:40:31 +0100

    adds axios to make requests to google api

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
   useEffect(() => {
-    console.log("New language or text");
+    await.post("https://translation.googleapis.com/language/translate/v2");
   }, [language, text]);
   return <div />;
 };
