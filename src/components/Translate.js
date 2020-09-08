84b6d88082c2a074b685237c6e17084b73267f05
Nelson <nbritob@hotmail.com>
2020-09-08 11:36:51 +0100

    adds Convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
+
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
 
 const options = [
   {
@@ -14,17 +17,33 @@ const options = [
     label: "Hindi",
     value: "hi",
   },
+  {
+    label: "Dutch",
+    value: "nl",
+  },
+
 ];
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
   return (
     <div>
+      <div className="ui form">
+        <div className="ui field">
+            <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
       <Dropdown
+        label="Select a language"
         selected={language}
         onSelectedChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
